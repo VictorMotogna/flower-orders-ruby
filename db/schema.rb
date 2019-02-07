3ca6664c283f059907852f6ba9b95717585a5f2e
Victor Motogna <vmotogna@gmail.com>
2019-02-07 10:34:55 +0200

    remove order tests and migrate order date to date type

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_06_142034) do
+ActiveRecord::Schema.define(version: 2019_02_07_083211) do
 
   create_table "orders", force: :cascade do |t|
-    t.string "date"
+    t.date "date"
     t.string "person"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
